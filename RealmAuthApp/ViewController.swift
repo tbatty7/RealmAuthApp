465f4bea3d2c0212fef4d65ebfbecfc51a3604ae
Tim Batty <tbatty7@gmail.com>
2025-09-08 12:24:45 -0400

    Initial Commit

---
+++ RealmAuthApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmAuthApp
+//
+//  Created by Timothy D Batty on 9/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RealmAuthAppTests/RealmAuthAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RealmAuthAppTests.swift
+//  RealmAuthAppTests
+//
+//  Created by Timothy D Batty on 9/8/25.
+//
+
+import Testing
+@testable import RealmAuthApp
+
+struct RealmAuthAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
